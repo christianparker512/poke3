00cd05d262261fe1347aa63eee256567851a0a7f
Christian Parker <christianparker512@gmail.com>
2021-07-08 14:34:39 -0500

    pokedex styling and creation of cards

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -1,16 +1,46 @@
 import React from 'react';
-import {AppBar, Toolbar} from "@material-ui/core";
+import {AppBar, Toolbar, Grid, Card, CardMedia, CardContent} from "@material-ui/core";
+import {makeStyles} from "@material-ui/core/styles"
 
-const Pokedex = () => {
+const useStyles = makeStyles ({
+    pokedexContainer: {
+        paddingTop: "20px",
+        paddingLeft: "50px",
+        paddingRight: "50px",
+        },
+    });
+const getPokemonCard = () => {
     return (
-        <>
-            <AppBar position="static">
-                <Toolbar />
-            </AppBar>
-            <div>this is our text</div>
-        </>
+        <Grid item xs={4}>
+            <Card>
+                <CardContent>Chris</CardContent>
+            </Card>
+
+        </Grid>
     )
+}
+const Pokedex = () => {
+    const classes = useStyles();
+    return (
+            <>
+                <AppBar position="static">
+                    <Toolbar />
+                </AppBar>
+                <Grid container spacing={2} className={classes.pokedexContainer}>
+                    {/*<Grid item xs={2}>*/}
+                    {/*this is item 1*/}
+                    {/* </Grid>*/}
+                    {/*<Grid item xs={2}>*/}
+                    {/* this is item 2*/}
+                    {/*</Grid>*/}
+                    {getPokemonCard()}
+                    {getPokemonCard()}
+                    {getPokemonCard()}
+                    {getPokemonCard()}
 
+                </Grid>
+                </>
+             );
+    };
 
-};
 export default Pokedex;
